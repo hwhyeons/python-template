73ebb3d9927c233dabde112b2aed3cf7261b1ec8
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-07-05 17:03:54 +0900

    Update get_data_with_url.py

--- web/get_data_url/get_data_with_url.py
+++ web/get_data_url/get_data_with_url.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
+from urllib.parse import quote
 
 url = 'http://localhost:8080/getAllWorkByDate?date=2023-07-03'
+# 실행하고자 하는 url에 한글이 포함되어있는 경우, quote("내용") 이렇게 보내면 됨
 f = urlopen(url)
 myfile = f.read()
 print(myfile)
