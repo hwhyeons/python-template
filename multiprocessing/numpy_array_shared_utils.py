3781cbadd4fb7e26a49b3b84ea41467fc95ea57c
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-11-14 19:57:27 +0900

    Update numpy_array_shared_utils.py
    [FIX] 딕셔너리 공유 안되는 문제 임시로 해결

--- multiprocessing/numpy_array_shared_utils.py
+++ multiprocessing/numpy_array_shared_utils.py
@@ -1,21 +1,56 @@
+import time
+
 import numpy as np
 from numpy import ndarray
 
+from multiprocessing.shared_memory import SharedMemory
 
 # 전역으로 공유 메모리 유지
-memory_name_dict = dict()
+# tmp_List에 넣는건 자동할당해제 되지 않도록 위함 (참조 카운트 유지)
+# 원래 이 딕셔너리도 공유 변수로 하는게 더 나아보이지만 임시로 이렇게 해놓음
+tmp_dict = dict()
 
 def make_shared_memory(name: str, numpy_shape: tuple, dtype: np, element_type_size: int):
-    global memory_name_dict
-    if name in memory_name_dict:
-        raise Exception(f"이미 존재하는 공유 메모리 이름. name : {name}")
     np_zero_likes_frame = np.zeros(numpy_shape, dtype=dtype)
     n_bytes = np_zero_likes_frame.nbytes
     from multiprocessing.shared_memory import SharedMemory
-    shared_screen_memory: SharedMemory = SharedMemory(name=name, size=n_bytes, create=True)
-    memory_name_dict[name] = shared_screen_memory
-    return shared_screen_memory
+    shared_memory: SharedMemory = SharedMemory(name=name, size=n_bytes, create=True)
+    tmp_dict[name] = shared_memory
+    return shared_memory
 
+def set_flag_bit(shared_memory_name: str, idx: int, flag: int | bool):
+    """
+    flag비트를 설정하는 함수
+    :param shared_memory_name:
+    :return:
+    """
+
+
+    if flag == True:
+        flag = 1
+    elif flag == False:
+        flag = 0
+
+    shared_memory = SharedMemory(shared_memory_name)
+    shared_memory.buf[idx] = flag
+    global tmp_dict
+    tmp_dict[shared_memory_name] = shared_memory
+
+
+def get_flag_bit(shared_memory_name: str, idx: int):
+    """
+    flag비트를 가져오는 함수
+    :param shared_memory_name:
+    :return:
+    """
+
+    shared_memory = SharedMemory(shared_memory_name)
+    global tmp_dict
+    tmp_dict[shared_memory_name] = shared_memory
+    return shared_memory.buf[idx]
+
+
+s_time_before_print = time.time()
 def copy_ndarray_to_shared_memory(shared_memory_name: str, numpy_array : ndarray, element_type_size: int):
     """
     참고
@@ -27,31 +62,28 @@ def copy_ndarray_to_shared_memory(shared_memory_name: str, numpy_array : ndarray
     :param element_type_size:
     :return:
     """
-    if shared_memory_name not in memory_name_dict:
-        raise Exception(f"존재하지 않는 공유 메모리 이름 -> make_shared_memory를 통해 먼저 공유 메모리 영역 설정 필요.\n\t"
-                        f"name : {shared_memory_name}")
 
-    print("복사할 ndarray shape : ",numpy_array.shape)
+    global s_time_before_print
+    if time.time()-s_time_before_print > 1:
+        print("복사할 ndarray shape : ",numpy_array.shape)
+        s_time_before_print = time.time()
 
     from multiprocessing.shared_memory import SharedMemory
     # shared_memory = SharedMemory(shared_memory_name)
-    shared_memory = memory_name_dict[shared_memory_name]
+    shared_memory = SharedMemory(shared_memory_name)
+    global tmp_dict
+    tmp_dict[shared_memory_name] = shared_memory
     shared_memory.buf[:numpy_array.nbytes] = numpy_array.flatten()
 
-def get_numpy_array_from_shared_memory(shared_memory_name: str, numpy_array_shape: tuple,dtype, element_type_size: int):
+def get_numpy_array_from_shared_memory(shared_memory_name: str, numpy_array_shape: tuple,dtype):
     """
     !!! shape에서 height가 앞으로 와야되는 것 주의 !!
     :param numpy_array_shape : (height,width,channel)
     :return:
     """
-    if shared_memory_name not in memory_name_dict:
-        raise Exception(f"존재하지 않는 공유 메모리 이름 -> make_shared_memory를 통해 먼저 공유 메모리 영역 설정 필요.\n\t"
-                        f"name : {shared_memory_name}")
-
-    from multiprocessing.shared_memory import SharedMemory
-    # shared_memory = SharedMemory(shared_memory_name)
-    shared_memory = memory_name_dict[shared_memory_name]
-    # 만약 크키 오류가 난다면 공유 메모리를 만든 상황의 type과 현재 받아오는 타입 불일치인 것
+    shared_memory = SharedMemory(shared_memory_name)
+    global tmp_dict
+    tmp_dict[shared_memory_name] = shared_memory
     shape_length = 1
     for i in numpy_array_shape:
         shape_length *= i
