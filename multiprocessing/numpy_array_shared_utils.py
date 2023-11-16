097746a402e390c179a6fd96c940ed176e480f6d
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-11-16 22:24:58 +0900

    Update numpy_array_shared_utils.py
    임시 딕셔너리 삭제 및 이름을 통해 인스턴스 재생성이 아니라 공유메모리 인스턴스 받아와서 수행하도록 수정

--- multiprocessing/numpy_array_shared_utils.py
+++ multiprocessing/numpy_array_shared_utils.py
@@ -5,20 +5,30 @@ from numpy import ndarray
 
 from multiprocessing.shared_memory import SharedMemory
 
+"""
+멀티프로세스를 사용해서 프로세스별 사용법
+
+1. 중복 실행 되거나 중복 import가 될 가능성이 없는 시작 py파일에서 공유 메모리 영역 생성 (make_shared_memory() 호출)
+2. numpy_array를 공유할 경우, 공유 메모리 영역에 데이터를 넣을 때는 copy_ndarray_to_shared_memory() 호출하고
+    공유 메모리 영역에서 데이터를 가져올 때는 get_numpy_array_from_shared_memory() 호출해서 사용
+3. 단순하게 flag를 공유하고 싶다면,
+    set_flag_bit() 호출해서 flag를 설정하고
+    get_flag_bit() 호출해서 flag를 가져오면 됨  
+ 
+"""
+
 # 전역으로 공유 메모리 유지
-# tmp_List에 넣는건 자동할당해제 되지 않도록 위함 (참조 카운트 유지)
-# 원래 이 딕셔너리도 공유 변수로 하는게 더 나아보이지만 임시로 이렇게 해놓음
-tmp_dict = dict()
 
 def make_shared_memory(name: str, numpy_shape: tuple, dtype: np, element_type_size: int):
     np_zero_likes_frame = np.zeros(numpy_shape, dtype=dtype)
     n_bytes = np_zero_likes_frame.nbytes
     from multiprocessing.shared_memory import SharedMemory
     shared_memory: SharedMemory = SharedMemory(name=name, size=n_bytes, create=True)
-    tmp_dict[name] = shared_memory
+    print(f"공유 메모리 생성 : {name} : {shared_memory.buf.nbytes}")
     return shared_memory
+    # shared_memory.close()
 
-def set_flag_bit(shared_memory_name: str, idx: int, flag: int | bool):
+def set_flag_bit(shared_memory: SharedMemory, idx: int, flag: int | bool):
     """
     flag비트를 설정하는 함수
     :param shared_memory_name:
@@ -31,27 +41,26 @@ def set_flag_bit(shared_memory_name: str, idx: int, flag: int | bool):
     elif flag == False:
         flag = 0
 
-    shared_memory = SharedMemory(shared_memory_name)
+    # shared_memory = SharedMemory(shared_memory_name)
     shared_memory.buf[idx] = flag
-    global tmp_dict
-    tmp_dict[shared_memory_name] = shared_memory
+    # shared_memory.close()
 
 
-def get_flag_bit(shared_memory_name: str, idx: int):
+def get_flag_bit(shared_memory: SharedMemory, idx: int):
     """
     flag비트를 가져오는 함수
     :param shared_memory_name:
     :return:
     """
 
-    shared_memory = SharedMemory(shared_memory_name)
-    global tmp_dict
-    tmp_dict[shared_memory_name] = shared_memory
-    return shared_memory.buf[idx]
+    # shared_memory = SharedMemory(shared_memory_name)
+    rt = shared_memory.buf[idx]
+    # shared_memory.close()
+    return rt
 
 
 s_time_before_print = time.time()
-def copy_ndarray_to_shared_memory(shared_memory_name: str, numpy_array : ndarray, element_type_size: int):
+def copy_ndarray_to_shared_memory(shared_memory: SharedMemory, numpy_array : ndarray, element_type_size: int):
     """
     참고
                             # print("shared_memory.buf.nbytes : ",shared_memory.buf.nbytes)
@@ -63,29 +72,28 @@ def copy_ndarray_to_shared_memory(shared_memory_name: str, numpy_array : ndarray
     :return:
     """
 
-    global s_time_before_print
-    if time.time()-s_time_before_print > 1:
-        print("복사할 ndarray shape : ",numpy_array.shape)
-        s_time_before_print = time.time()
+    # global s_time_before_print
+    # if time.time()-s_time_before_print > 1:
+    #     print("복사할 ndarray shape : ",numpy_array.shape)
+    #     s_time_before_print = time.time()
 
     from multiprocessing.shared_memory import SharedMemory
     # shared_memory = SharedMemory(shared_memory_name)
-    shared_memory = SharedMemory(shared_memory_name)
-    global tmp_dict
-    tmp_dict[shared_memory_name] = shared_memory
+    # shared_memory = SharedMemory(shared_memory_name)
     shared_memory.buf[:numpy_array.nbytes] = numpy_array.flatten()
+    # shared_memory.close()
 
-def get_numpy_array_from_shared_memory(shared_memory_name: str, numpy_array_shape: tuple,dtype):
+def get_numpy_array_from_shared_memory(shared_memory: SharedMemory, numpy_array_shape: tuple,dtype):
     """
     !!! shape에서 height가 앞으로 와야되는 것 주의 !!
     :param numpy_array_shape : (height,width,channel)
     :return:
     """
-    shared_memory = SharedMemory(shared_memory_name)
-    global tmp_dict
-    tmp_dict[shared_memory_name] = shared_memory
+    # shared_memory = SharedMemory(shared_memory_name)
     shape_length = 1
     for i in numpy_array_shape:
         shape_length *= i
     rt_array = np.reshape(shared_memory.buf[:shape_length],newshape=numpy_array_shape)
+    # shared_memory.close()
+
     return rt_array
