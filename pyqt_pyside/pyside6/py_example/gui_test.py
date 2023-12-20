7a68bd9ae81b8ec8f3fe7d0e061d94f7ea62f5da
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-12-20 12:15:53 +0900

    Create gui_test.py
    add file

---
+++ pyqt_pyside/pyside6/py_example/gui_test.py
@@ -0,0 +1,23 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+# ui_test_window를 ui파일에서 python 코드로 바꾼 py파일 경로를 불러오기
+
+from ui_test_window import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+    def actionTest(self):
+        print("action test..")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
