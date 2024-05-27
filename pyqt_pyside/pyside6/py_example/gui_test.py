53f76922e6947619e90f832f9d6006c6fe491619
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-05-27 22:25:06 +0900

    Update gui_test.py

--- pyqt_pyside/pyside6/py_example/gui_test.py
+++ pyqt_pyside/pyside6/py_example/gui_test.py
@@ -2,6 +2,7 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 # ui_test_window를 ui파일에서 python 코드로 바꾼 py파일 경로를 불러오기
+# QMainWindow가 아니라 QDialog의 경우에는 타입만 변경해주면 됨
 
 from ui_test_window import Ui_MainWindow
 
